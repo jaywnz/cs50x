7cb9d178f44a6bdb86564b1e3b0a3e62222a45ea
jaywnz <jaywnz@users.noreply.github.com>
2020-04-28 08:50:47 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -61,7 +61,7 @@ int get_first_digit(long n, int len) // returns first digit of card number
 {
     int first = 0;
     int counter = 0;
-    while(counter < len) // extracts the first digit
+    while (counter < len) // extracts the first digit
     {
         first = n % 10;
         n /= 10; // moves up a number
@@ -74,7 +74,7 @@ int get_second_digit(long n, int len) // returns first digit of card number
 {
     int second = 0;
     int counter = 0;
-    while(counter < len - 1) // extracts the second digit
+    while (counter < len - 1) // extracts the second digit
     {
         second = n % 10;
         n /= 10; // moves up a number
@@ -83,39 +83,28 @@ int get_second_digit(long n, int len) // returns first digit of card number
     return second;
 }
 
-bool get_checksum(long n, int len) // returns checksum pass/fail
+bool get_checksum(long n, int len)
 {
-    int x = 10;
     int even_total = 0;
     int odd_total = 0;
-    int mod0 = 0;
-    int mod1 = 0;
-    long digit = 0; // needs to be more precise for division operation
-    for (int i = 0; i < len; i++)
+    for (int counter = 0; counter < len; counter++)
     {
-        if ((i + 1) % 2 == 0) // if the counter is in an even position
+        int digit = n % 10;
+        n /= 10;
+        if (counter % 2 != 0) // odd counter (counter starts at 0)
         {
-            mod0 = n % x; // gets digits from the end of card no. according to x
-            mod1 = n % (x / 10); // gets numbers after digit above
-            digit = mod0 - mod1; // extracts number with trailing zeroes
-            digit = (digit / x) * 10; // drops trailing zeroes
-            int sub_total = digit * 2; // doubles the digit
-            even_total += sub_total % 10; // deals with two digit number by adding last digit
-            even_total += (sub_total - (sub_total % 10)) / 10; // adds first digit of two digit number
+            digit *= 2;
+            int last_digit = digit % 10;
+            int first_digit = digit / 10; // will return 0 for single digits
+            odd_total += last_digit + first_digit;
         }
-        else // if the counter is odd
+        else // even counter
         {
-            mod0 = n % x;
-            mod1 = n % (x / 10);
-            digit = mod0 - mod1;
-            digit = (digit / x) * 10;
-            odd_total += digit;
+            even_total += digit;
         }
-        x *= 10;
     }
-    int total = 0;
-    total = even_total + odd_total;
-    if (total % 2 == 0) //
+    int total = even_total + odd_total;
+    if (total % 2 == 0)
     {
         return true;
     }
@@ -124,3 +113,53 @@ bool get_checksum(long n, int len) // returns checksum pass/fail
         return false;
     }
 }
+
+// bool get_checksum(long n, int len) // returns checksum pass/fail
+// {
+//     int x = 10;
+//     int even_total = 0;
+//     int odd_total = 0;
+//     long mod0 = 0;
+//     long mod1 = 0;
+//     int digit = 0.0; // needs to be more precise for division operation
+//     for (int i = 0; i < len; i++)
+//     {
+//         if ((i + 1) % 2 == 0) // if the counter is in an even position
+//         {
+//             mod0 = n % x; // gets digits from the end of card no. according to x
+//             printf("%li\n", mod0);
+//             mod1 = n % (x / 10); // gets numbers after digit above
+//             printf("%li\n", mod1);
+//             digit = mod0 - mod1; // extracts number with trailing zeroes
+//             printf("%i\n", digit);
+//             digit = (digit / x) * 10; // drops trailing zeroes
+//             printf("%i\n", digit);
+//             int sub_total = digit * 2; // doubles the digit
+//             even_total += sub_total % 10; // deals with two digit number by adding last digit
+//             even_total += (sub_total - (sub_total % 10)) / 10; // adds first digit of two digit number
+//         }
+//         else // if the counter is odd
+//         {
+//             mod0 = n % x;
+//             printf("%li\n", mod0);
+//             mod1 = n % (x / 10);
+//             printf("%li\n", mod1);
+//             digit = mod0 - mod1;
+//             printf("%i\n", digit);
+//             digit = (digit / x) * 10;
+//             printf("%i\n", digit);
+//             odd_total += digit;
+//         }
+//         x *= 10;
+//     }
+//     int total = 0;
+//     total = even_total + odd_total;
+//     if (total % 2 == 0) //
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
