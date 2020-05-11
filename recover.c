5286cc196271d2f75cd69fc5abf62a20be93a2f3
jaywnz <jaywnz@users.noreply.github.com>
2020-05-11 20:42:47 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -29,44 +29,40 @@ int main(int argc, char *argv[])
     char *filename = malloc(8 * sizeof(char));
 
     // Initialise counter for file naming
-    int jpeg_count = 0;
+    int jpeg_count = -1;
 
     FILE *img = NULL;
 
     // Repeat until fread cannot return 512 (denoting EOF)
     while (fread(buffer, 1, 512, card) == 512)
     {
-
         // Find start of new JPEG by header
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             // Count each JPEG found
             jpeg_count++;
-            // Print ###.jpg filename to a string
-            sprintf(filename, "%03i.jpg", jpeg_count);
-            // Open new file with generated filename for writing
-            img = fopen(filename, "w");
+
             // Check if it is the first JPEG
-            if (jpeg_count == 1)
+            if (jpeg_count == 0)
             {
-                // Write buffer to file
+                // Generate filename, open new file and write buffer
+                sprintf(filename, "%03i.jpg", jpeg_count);
+                img = fopen(filename, "w");
                 fwrite(buffer, 1, 512, img);
             }
             else
             {
                 // Close previous file
                 fclose(img);
-                // Print ###.jpg filename to a string
+                // Generate filename, open new file and write buffer
                 sprintf(filename, "%03i.jpg", jpeg_count);
-                // Open new file with generated filename for writing
                 img = fopen(filename, "w");
-                // Write buffer to file
                 fwrite(buffer, 1, 512, img);
             }
         }
         else if (jpeg_count > 0)
         {
-            // If already found JPEG, keep writing to it
+            // Keep writing to JPEG already open
             fwrite(buffer, 1, 512, img);
         }
     }
