cb1a8a29c3d5105756096260d8a4a83ff3ad31cb
jaywnz <jaywnz@users.noreply.github.com>
2020-05-23 10:38:23 +0000

    automated commit by submit50

---
+++ 13.sql
@@ -0,0 +1,9 @@
+SELECT name FROM people
+WHERE people.id IN
+(SELECT stars.person_id FROM stars
+WHERE stars.movie_id IN
+(SELECT stars.movie_id FROM stars
+WHERE stars.person_id =
+(SELECT people.id FROM people
+WHERE name = "Kevin Bacon" AND birth = 1958)))
+AND name IS NOT "Kevin Bacon";
